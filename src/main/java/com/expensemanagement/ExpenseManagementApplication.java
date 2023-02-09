d7ee72b1a66d5b708ceaed226fdd8e8e53cde6b8
SumitEM <sumitsoni9575@gmail.com>
2023-02-09 13:08:34 +0530

    create and update ExpenseManagement for HRMS-8

--- src/main/java/com/expensemanagement/ExpenseManagementApplication.java
+++ src/main/java/com/expensemanagement/ExpenseManagementApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExpenseManagementApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseManagementApplication.class, args);
 	}
-
 }
