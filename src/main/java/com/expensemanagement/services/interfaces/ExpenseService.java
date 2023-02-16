def9078b092895b10a5a2754de7be24072da78f0
SumitEM <sumitsoni9575@gmail.com>
2023-02-16 20:11:12 +0530

    HRMS-18  Loging & ExceptionHandling added

--- src/main/java/com/expensemanagement/services/interfaces/ExpenseService.java
+++ src/main/java/com/expensemanagement/services/interfaces/ExpenseService.java
@@ -6,4 +6,6 @@ public interface ExpenseService {
     public ExpenseItems createExpenses(ExpenseItems expenseItems);
 
     public String updateExpense(int id,ExpenseItems expenseItems);
+
+    public ExpenseItems getExpenseById(int id) throws NoSuchFieldException;
 }
---
+++ src/main/java/com/expensemanagement/utilities/errorResponseUtilities/FieldErrors.java
@@ -0,0 +1,19 @@
+package com.expensemanagement.utilities.errorResponseUtilities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+@AllArgsConstructor
+public class FieldErrors {
+    private int status;
+    private String message;
+    private Map<String, String> field_errors;
+
+    public FieldErrors(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
