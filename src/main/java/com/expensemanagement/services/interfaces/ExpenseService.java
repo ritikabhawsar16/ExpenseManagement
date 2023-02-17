5134dd27c5fcccef9e3dc191a90f7ed5ecd26fa5
DhananjayBobde <113025710+DhananjayBobde@users.noreply.github.com>
2023-02-17 15:39:22 +0530

    add outbound api

--- src/main/java/com/expensemanagement/services/interfaces/ExpenseService.java
+++ src/main/java/com/expensemanagement/services/interfaces/ExpenseService.java
@@ -1,6 +1,7 @@
 package com.expensemanagement.services.interfaces;
 
 import com.expensemanagement.models.ExpenseItems;
+import com.expensemanagement.models.ExpenseOutbound;
 
 public interface ExpenseService {
     public ExpenseItems createExpenses(ExpenseItems expenseItems);
@@ -8,4 +9,8 @@ public interface ExpenseService {
     public String updateExpense(int id,ExpenseItems expenseItems);
 
     public ExpenseItems getExpenseById(int id) throws NoSuchFieldException;
+
+	public String saveOutboundExpense(ExpenseOutbound expenseOutbound);
+
+	public String updateOutboundExpense(ExpenseOutbound expenseOutbound);
 }
