ecaa924750e8f07f0aca3111f35785be7f2e4b32
anandpal-adt <anandpalsingh.adt@gmail.com>
2023-02-20 17:09:22 +0530

    added save clinet Info API and Get Client Info API

--- src/main/java/com/expensemanagement/controllers/ClientInformationController.java
+++ src/main/java/com/expensemanagement/controllers/ClientInformationController.java
@@ -2,12 +2,15 @@ package com.expensemanagement.controllers;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.expensemanagement.models.ClientInformation;
@@ -15,19 +18,27 @@ import com.expensemanagement.models.ResponseModel;
 import com.expensemanagement.services.interfaces.ClientInformationService;
 
 @RestController
+@RequestMapping("/clientInfo")
 public class ClientInformationController {
 	
     @Autowired
     private ClientInformationService informationService;
 	
+    private static final Logger LOGGER = LogManager.getLogger(ClientInformationController.class);
+
+    
     @PostMapping("/saveClientInfo")
     public ResponseEntity<ResponseModel> saveClientInfo(@RequestBody ClientInformation clientInfo) {
+        LOGGER.info("Expense Management: clientInfo : saveClientInfo Info level log message");
+
     	ResponseModel model=informationService.saveClientInfo(clientInfo);
         return new ResponseEntity<>(model, HttpStatus.OK);
     }
 	
 	@GetMapping("/getAllClientInfo")
 	public ResponseEntity<List<ClientInformation>> getAllClientInfo() {
+        LOGGER.info("Expense Management: getAllClientInfo : getAllClientInfo Info level log message");
+		
 		List<ClientInformation> information=informationService.getAllClientInfo();
 		return new ResponseEntity<>(information, HttpStatus.OK);
 	}
