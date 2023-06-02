e4dcb72a1064210c21dfe82c9f406957be1e7039
Ajay Mishra <ajaymishra.adt@gmail.com>
2023-06-02 12:12:55 +0530

    changes for permission RBAC

--- src/main/java/com/expensemanagement/controllers/ClientInformationController.java
+++ src/main/java/com/expensemanagement/controllers/ClientInformationController.java
@@ -9,6 +9,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,44 +25,46 @@ import com.expensemanagement.services.interfaces.ClientInformationService;
 @RestController
 @RequestMapping("/clientInfo")
 public class ClientInformationController {
-	
-    @Autowired
-    private ClientInformationService informationService;
-	
-    private static final Logger LOGGER = LogManager.getLogger(ClientInformationController.class);
 
-    
-    @PostMapping("/saveClientInfo")
-    public ResponseEntity<ResponseModel> saveClientInfo(@RequestBody ClientInformation clientInfo) {
-        LOGGER.info("Expense Management: clientInfo : saveClientInfo Info level log message");
+	@Autowired
+	private ClientInformationService informationService;
 
-    	ResponseModel model=informationService.saveClientInfo(clientInfo);
-        return new ResponseEntity<>(model, HttpStatus.OK);
-    }
-	
+	private static final Logger LOGGER = LogManager.getLogger(ClientInformationController.class);
+
+	@PreAuthorize("@auth.allow('ROLE_ADMIN')")
+	@PostMapping("/saveClientInfo")
+	public ResponseEntity<ResponseModel> saveClientInfo(@RequestBody ClientInformation clientInfo) {
+		LOGGER.info("Expense Management: clientInfo : saveClientInfo Info level log message");
+
+		ResponseModel model = informationService.saveClientInfo(clientInfo);
+		return new ResponseEntity<>(model, HttpStatus.OK);
+	}
+
+	@PreAuthorize("@auth.allow('ROLE_ADMIN')")
 	@GetMapping("/getAllClientInfo")
 	public ResponseEntity<List<ClientInformation>> getAllClientInfo() {
-        LOGGER.info("Expense Management: getAllClientInfo : getAllClientInfo Info level log message");
-		
-		List<ClientInformation> information=informationService.getAllClientInfo();
+		LOGGER.info("Expense Management: getAllClientInfo : getAllClientInfo Info level log message");
+
+		List<ClientInformation> information = informationService.getAllClientInfo();
 		return new ResponseEntity<>(information, HttpStatus.OK);
 	}
+
+	@PreAuthorize("@auth.allow('ROLE_ADMIN')")
 	@PutMapping("/updateClientInfo/{id}")
 	public ResponseEntity<String> updateClientInfo(@PathVariable("id") int id,
-			@RequestBody ClientInformation clientInformation,HttpServletRequest request) {
-		 LOGGER.info("API Call From IP: " + request.getRemoteHost());
-		return new ResponseEntity<>(informationService.updateClientInfo(id, clientInformation),
-				HttpStatus.OK);
+			@RequestBody ClientInformation clientInformation, HttpServletRequest request) {
+		LOGGER.info("API Call From IP: " + request.getRemoteHost());
+		return new ResponseEntity<>(informationService.updateClientInfo(id, clientInformation), HttpStatus.OK);
 	}
-	//***** HRMS-54 START Added new handler method to get Client information using client id  *****
+
+	@PreAuthorize("@auth.allow('ROLE_ADMIN')")
 	@GetMapping("/getClientInfoById/{clientId}")
-	public ResponseEntity<ClientInformation> getClientInformationById(@PathVariable("clientId") int clientId){
+	public ResponseEntity<ClientInformation> getClientInformationById(@PathVariable("clientId") int clientId) {
 		LOGGER.info("Expense Management: ClientInformationController: getClientInformationById info level log message");
 		ClientInformation clientInfo = informationService.getClientInfoById(clientId);
-		if(clientInfo == null)
+		if (clientInfo == null)
 			return ResponseEntity.notFound().build();
-		
+
 		return ResponseEntity.ok(clientInfo);
 	}
-	//***** HRMS-54 END ***
 }
--- src/main/java/com/expensemanagement/controllers/ExpenseController.java
+++ src/main/java/com/expensemanagement/controllers/ExpenseController.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import com.expensemanagement.models.ExpenseItems;
@@ -20,55 +21,66 @@ import java.util.List;
 @RestController
 public class ExpenseController {
 
-    private static final Logger LOGGER = LogManager.getLogger(ExpenseController.class);
+	private static final Logger LOGGER = LogManager.getLogger(ExpenseController.class);
 
-    @Autowired
-    private ExpenseService expenseService;
+	@Autowired
+	private ExpenseService expenseService;
 
-    @GetMapping("/getAllExpenses")
-    public ResponseEntity<List<ExpenseItems>> getAllExpenses(HttpServletRequest request) {
-        LOGGER.info("API Call From IP: " + request.getRemoteHost());
-        List<ExpenseItems> expenses = expenseService.getAllExpenses();
-        return new ResponseEntity<>(expenses, HttpStatus.OK);
-    }
+	@GetMapping("/getAllExpenses")
+	public ResponseEntity<List<ExpenseItems>> getAllExpenses(HttpServletRequest request) {
+		LOGGER.info("API Call From IP: " + request.getRemoteHost());
+		List<ExpenseItems> expenses = expenseService.getAllExpenses();
+		return new ResponseEntity<>(expenses, HttpStatus.OK);
+	}
 
-    @PostMapping("/createExpenses")
-    public ResponseEntity<ExpenseItems> createExpenses(@RequestBody @Valid ExpenseItems expenseItems, HttpServletRequest request) {
-        LOGGER.info("API Call From IP: " + request.getRemoteHost());
-        ExpenseItems expenses=expenseService.createExpenses(expenseItems);
-        return new ResponseEntity<>(expenses, HttpStatus.OK);
-    }
+	@PreAuthorize("@auth.allow('ROLE_ADMIN')")
+	@PostMapping("/createExpenses")
+	public ResponseEntity<ExpenseItems> createExpenses(@RequestBody @Valid ExpenseItems expenseItems,
+			HttpServletRequest request) {
+		LOGGER.info("API Call From IP: " + request.getRemoteHost());
+		ExpenseItems expenses = expenseService.createExpenses(expenseItems);
+		return new ResponseEntity<>(expenses, HttpStatus.OK);
+	}
 
-    @PutMapping("/updateExpense/{id}")
-    public ResponseEntity<String> updateExpense(@PathVariable("id") int id,@RequestBody ExpenseItems expenseItems, HttpServletRequest request){
-        LOGGER.info("API Call From IP: " + request.getRemoteHost());
-        return  new ResponseEntity<>(this.expenseService.updateExpense(id,expenseItems), HttpStatus.OK);
-    }
+	@PreAuthorize("@auth.allow('ROLE_ADMIN')")
+	@PutMapping("/updateExpense/{id}")
+	public ResponseEntity<String> updateExpense(@PathVariable("id") int id, @RequestBody ExpenseItems expenseItems,
+			HttpServletRequest request) {
+		LOGGER.info("API Call From IP: " + request.getRemoteHost());
+		return new ResponseEntity<>(this.expenseService.updateExpense(id, expenseItems), HttpStatus.OK);
+	}
 
-    @GetMapping("/getExpenseById/{id}")
-    public ResponseEntity<ExpenseItems> getExpenseById(@PathVariable("id") int id, HttpServletRequest request) throws NoSuchFieldException {
-        LOGGER.info("API Call From IP: " + request.getRemoteHost());
-        return new ResponseEntity<>(expenseService.getExpenseById(id), HttpStatus.OK);
-    }
-    
-    @PostMapping("/saveOutboundExpense")
-    public ResponseEntity<String> saveOutboundExpense(@RequestBody ExpenseOutbound expenseOutbound,HttpServletRequest request){
-    	
-    	  LOGGER.info("API Call From IP: " + request.getRemoteHost());
-    	return new ResponseEntity<>(expenseService.saveOutboundExpense(expenseOutbound),HttpStatus.OK);
-    }
-    
-    @PutMapping("updateOutboundExpense")
-    public ResponseEntity<String> updateOutboundExpense(@RequestBody ExpenseOutbound expenseOutbound,HttpServletRequest request){
-  	  LOGGER.info("API Call From IP: " + request.getRemoteHost());
-  	return new ResponseEntity<>(expenseService.updateOutboundExpense(expenseOutbound),HttpStatus.OK);
-  }
+	@PreAuthorize("@auth.allow('ROLE_ADMIN')")
+	@GetMapping("/getExpenseById/{id}")
+	public ResponseEntity<ExpenseItems> getExpenseById(@PathVariable("id") int id, HttpServletRequest request)
+			throws NoSuchFieldException {
+		LOGGER.info("API Call From IP: " + request.getRemoteHost());
+		return new ResponseEntity<>(expenseService.getExpenseById(id), HttpStatus.OK);
+	}
 
-    @GetMapping("/getExpenseByDateRange")
-    public ResponseEntity<List<ExpenseItems>> getExpenseByDateRange(@RequestParam("startDate") String from, @RequestParam("endDate") String to, HttpServletRequest request) throws NoSuchFieldException, ParseException {
-        LOGGER.info("API Call From IP: " + request.getRemoteHost());
-        return new ResponseEntity<>(expenseService.getExpenseByDateRange(from, to), HttpStatus.OK);
-    }
-    
+	@PreAuthorize("@auth.allow('ROLE_ADMIN')")
+	@PostMapping("/saveOutboundExpense")
+	public ResponseEntity<String> saveOutboundExpense(@RequestBody ExpenseOutbound expenseOutbound,
+			HttpServletRequest request) {
+		LOGGER.info("API Call From IP: " + request.getRemoteHost());
+		return new ResponseEntity<>(expenseService.saveOutboundExpense(expenseOutbound), HttpStatus.OK);
+	}
+
+	@PreAuthorize("@auth.allow('ROLE_ADMIN')")
+	@PutMapping("updateOutboundExpense")
+	public ResponseEntity<String> updateOutboundExpense(@RequestBody ExpenseOutbound expenseOutbound,
+			HttpServletRequest request) {
+		LOGGER.info("API Call From IP: " + request.getRemoteHost());
+		return new ResponseEntity<>(expenseService.updateOutboundExpense(expenseOutbound), HttpStatus.OK);
+	}
+
+	@PreAuthorize("@auth.allow('ROLE_ADMIN')")
+	@GetMapping("/getExpenseByDateRange")
+	public ResponseEntity<List<ExpenseItems>> getExpenseByDateRange(@RequestParam("startDate") String from,
+			@RequestParam("endDate") String to, HttpServletRequest request)
+			throws NoSuchFieldException, ParseException {
+		LOGGER.info("API Call From IP: " + request.getRemoteHost());
+		return new ResponseEntity<>(expenseService.getExpenseByDateRange(from, to), HttpStatus.OK);
+	}
 
 }
