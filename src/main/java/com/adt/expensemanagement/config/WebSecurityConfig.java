83b5b7d49b23afc5c43345524e955b2440ba3a75
ankitdeshmu <ankitdeshmukh.adt@gmail.com>
2024-04-19 10:53:23 +0530

    add  new parameter in permit

--- src/main/java/com/adt/expensemanagement/config/WebSecurityConfig.java
+++ src/main/java/com/adt/expensemanagement/config/WebSecurityConfig.java
@@ -3,22 +3,26 @@ package com.adt.expensemanagement.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig {
+public class WebSecurityConfig  {
 
 	private String allowUrlPattern = "/expenseManagement/**";
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-		http.authorizeRequests().requestMatchers(allowUrlPattern,"/swagger-ui.html").permitAll();
+		http.authorizeRequests().requestMatchers(allowUrlPattern,"/swagger-ui.html/**").permitAll();
 		return http.build();
 	}
+	
+	 
 
 }
